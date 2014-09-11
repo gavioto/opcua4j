eb8e4ddb5ff50d3b726203d03fc3d8400f2df7a6
harald.hofstaetter@gmail.com <harald.hofstaetter@gmail.com@415f11c9-9457-f4ba-dee3-a3dc3c0eec9d>
2014-09-11 14:14:41 +0000

    started implementing SubscriptionsManager to hande subscriptions and monitored items

--- opcua4j/src/bpi/most/opcua/server/core/subscription/SubscriptionManager.java
+++ opcua4j/src/bpi/most/opcua/server/core/subscription/SubscriptionManager.java
@@ -1,12 +1,24 @@
 package bpi.most.opcua.server.core.subscription;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.opcfoundation.ua.builtintypes.UnsignedInteger;
+import org.opcfoundation.ua.core.CreateSubscriptionRequest;
+import org.opcfoundation.ua.core.DeleteSubscriptionsRequest;
 import org.opcfoundation.ua.core.PublishRequest;
 import org.opcfoundation.ua.core.PublishResponse;
+import org.opcfoundation.ua.core.SetPublishingModeRequest;
 import org.opcfoundation.ua.transport.EndpointServiceRequest;
+
+import bpi.most.opcua.server.handler.SessionServiceHandler;
 
 
 public class SubscriptionManager {
 
+	private static final Logger LOG = Logger.getLogger(SubscriptionManager.class);
+	
 	/*
 	 * theory: OPC Unified Architecture book (the green one) 5.7.1, especially 5.7.1.1
 	 * 
@@ -25,9 +37,59 @@ public class SubscriptionManager {
 	 * 		publishReq.setREsponse(publish response)
 	 * }
 	 * 
-	 
-    
 	 */
+	
+	/**
+	 * counts up to generate unique subscription IDs
+	 */
+	private static int subscriptionIndex = 0;
+	
+	private Map<Integer, Subscription> subscriptions;
+	
+	public SubscriptionManager() {
+		subscriptions = new HashMap<Integer, Subscription>();
+	}
+
+	/**
+	 * creates the subscription and adds it to the
+	 * managed ones
+	 * @return
+	 */
+	public synchronized Subscription createSubscription(CreateSubscriptionRequest req){
+		Subscription subscription = new Subscription(subscriptionIndex++);
+		
+		//TODO validate all requested values
+		subscription.setLifetimeCount(req.getRequestedLifetimeCount().intValue());
+		subscription.setMaxKeepAliveCount(req.getRequestedMaxKeepAliveCount().intValue());
+		subscription.setMaxNotificationsPerPublish(req.getMaxNotificationsPerPublish().intValue());
+		subscription.setPriority(req.getPriority().intValue());
+		subscription.setPublishingEnabled(req.getPublishingEnabled());
+		subscription.setPublishingInterval(req.getRequestedPublishingInterval());
+		
+		subscriptions.put(subscription.getId(), subscription);
+		return subscription;
+	}
+	
+	public synchronized void setPublishingMode(SetPublishingModeRequest req){
+		for (UnsignedInteger id: req.getSubscriptionIds()){
+			if (subscriptions.containsKey(id)){
+				subscriptions.get(id).setPublishingEnabled(req.getPublishingEnabled());
+			}else{
+				LOG.debug("clients wants to set publishing mode for subscriptionid " + id + ", but does not exist");
+			}
+		}
+	}
+	
+	public synchronized void deleteSubscription(DeleteSubscriptionsRequest req){
+		for (UnsignedInteger id: req.getSubscriptionIds()){
+			if (subscriptions.containsKey(id)){
+				subscriptions.remove(id);
+			}else{
+				LOG.debug("clients wants to remove subscription with id " + id + ", but does not exist");
+			}
+		}
+	}
+	
 	
 	private void bla(){
 		//thats it
